daf430972774d22a940be7fb80875cdcc3a62f67
Utkarsh Yadav <utkarshyadav3078@gmail.com>
2026-04-27 14:05:11 +0530

    socket implemented

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
 import http from "node:http";
-import express from "express";
 import path from "node:path";
 
+import express from "express";
+import { Server } from "socket.io";
+
 async function main() {
+  const port = process.env.PORT || 8000;
+
   const app = express();
   const server = http.createServer(app);
 
-  const port = process.env.PORT || 8000;
-  app.use(express.static(path.resolve("./public")));
+  const io = new Server();
+  io.attach(server);
 
+  //Socket IO handler
+  io.on("connection", (socket) => {
+    console.log(`Socket connected`, { id: socket.id });
+
+    socket.on("client:checkbox:change", (data) => {
+      console.log(`[Socket:${socket.id}]:client:checkbox:change`, data);
+      io.emit("server:checkbox:change", data);
+    });
+  });
+
+  //express handlers
+  app.use(express.static(path.resolve("./public")));
   app.get("/health", (req, res) => res.json({ healthy: true }));
 
   server.listen(port, () => {
